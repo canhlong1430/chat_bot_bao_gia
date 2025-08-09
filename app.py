66e454297a0010aea26efd575d8d3449a3ce036e
canhlong1430 <73382249+canhlong1430@users.noreply.github.com>
2025-08-09 12:00:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ def download_file(filename):
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8006))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
